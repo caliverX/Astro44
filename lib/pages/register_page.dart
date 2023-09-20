b536a86a779f4973fb48e4d6b05a843877c1d87b
caliverX <143669043+caliverX@users.noreply.github.com>
2023-09-21 01:16:31 +0200

    update register_page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -8,7 +8,7 @@ import 'package:astro44/servieces/auth_service.dart';
 
 class RegisterPage extends StatefulWidget {
   final Function()? onTap;
-  RegisterPage({super.key, required this.onTap});
+  const RegisterPage({super.key, required this.onTap});
 
   @override
   State<RegisterPage> createState() => _RegisterPageState();
